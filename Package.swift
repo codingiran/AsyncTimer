65449ee1e5ae31803d1e819e6703b0784de4cc45
CodingIran <codingiran@gmail.com>
2025-05-20 20:52:49 +0800

    wip

---
+++ Package.swift
@@ -0,0 +1,35 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "AsyncTimer",
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v13),
+        .tvOS(.v13),
+        .watchOS(.v6),
+        .macCatalyst(.v13),
+        .visionOS(.v1),
+    ],
+
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "AsyncTimer",
+            targets: ["AsyncTimer"]
+        ),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "AsyncTimer"),
+        .testTarget(
+            name: "AsyncTimerTests",
+            dependencies: ["AsyncTimer"]
+        ),
+    ],
+    swiftLanguageModes: [.v6]
+)
